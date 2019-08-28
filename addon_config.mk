e2901a4cfb26b1a41483af3bec33dbe0f5e32716
asaiyuta <yuta@rhizomatiks.com>
2019-08-28 21:50:20 +0900

    initial commit 2.2.25

---
+++ addon_config.mk
@@ -0,0 +1,10 @@
+
+meta:
+	ADDON_NAME = ofxLibRealsense2
+	ADDON_DESCRIPTION = Add librealsense2 to the project for openFrameworks on xcode. https://github.com/IntelRealSense/librealsense
+	ADDON_AUTHOR = asaiyuta
+	ADDON_TAGS = "librealsense2"
+	ADDON_URL = https://github.com/asaiyuta/ofxLibRealsense2
+
+osx:
+	ADDON_LDFLAGS = -Xlinker -rpath -Xlinker @executable_path
---
+++ src/ofxLibRealsense2.h
@@ -0,0 +1,4 @@
+#ifndef ofxLibRealsense2_h
+#define ofxLibRealsense2_h
+#include <librealsense2/rs.hpp>
+#endif /* ofxLibRealsense2Mac_h */
